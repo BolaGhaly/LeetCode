9fd5fc3c693130754eb07fcf538945067812d4ab
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2023-07-12 10:41:53 -0400

    Time: 56 ms (94.99%), Space: 43.2 MB (29.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,10 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    let vals = {};
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j <= nums.length; j++) {
-            if (nums[i] + nums[j] === target) 
-                return [i, j]
+        if (target - nums[i] in vals) {
+            return [vals[target-nums[i]], i];
+        } else {
+            vals[nums[i]] = i;
         }
     }
+    return [];
 };
