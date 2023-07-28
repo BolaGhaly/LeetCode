8083f70659db26361530c690be934ef016545829
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2023-07-28 03:16:01 -0400

    Time: 73 ms (99.52%), Space: 55.3 MB (23.95%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let tempSum = 0;
+    
+    for (let i = 0; i < k; i++) {
+        tempSum += nums[i];
+    }
+    
+    tempSum = tempSum / k;
+    let maxAvg = tempSum;
+
+    for (let i = k; i < nums.length; i++) {
+        tempSum = ((tempSum * k) - nums[i - k] + nums[i]) / k;
+        maxAvg = Math.max(tempSum, maxAvg)
+    }
+    
+    return maxAvg;
+};
