c861cb8f470bed696130ead59cf481b045ae9a51
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2023-07-31 01:13:15 -0400

    Time: 51 ms (94.64%), Space: 44.2 MB (21.89%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -3,16 +3,10 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    let arrObj = {};
-    
-    for (let i = 0; i < arr.length; i++) {
-        arrObj[arr[i]] = (arrObj[arr[i]] || 0) + 1;
-    }
-        
-    for (let i = 0; i < arr.length; i++) {
-        let currNum = arr[i];
-        if (((currNum / 2) in arrObj || (currNum * 2) in arrObj) && currNum !== 0) return true;
-        if (currNum === 0 && arrObj[currNum] > 1) return true;
+    const arrObj = {};
+    for (let num of arr) {
+        if (arrObj[num / 2] || arrObj[num * 2]) return true;
+        arrObj[num] = true;
     }
     
     return false;
