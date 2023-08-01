0dd7b798b0bc6e0fde5f483cac847a2e72b81a45
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2023-08-01 11:14:40 -0400

    Time: 87 ms (81.07%), Space: 55.2 MB (33.58%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,18 +3,6 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    let i = 0;
-    let j = 1;
-    
-    while (i < j && i < nums.length - 1) {
-        if (nums[i] === nums[j]) return true;
-        if (j === nums.length - 1) {
-            i++;
-            j = i + 1;
-        } else {
-            j++;
-        }
-    }
-    
-    return false;
+    const s = new Set(nums);
+    return s.size !== nums.length;
 };
