f5f6186187b061f24f059008082671accc00b3ca
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2023-08-01 14:45:53 -0400

    Time: 71 ms (57.72%), Space: 46.3 MB (27.79%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,46 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    class reversePolishNotation {
+        constructor() {
+            this.tokensArr = [];
+        }
+        
+        push(val) {
+            this.tokensArr.push(val);
+        }
+        
+        getArithmeticResult() {
+            const operator = this.tokensArr.pop();
+            const num2 =  parseInt(this.tokensArr.pop());
+            const num1 =  parseInt(this.tokensArr.pop());
+
+            if (operator === "+") this.tokensArr.push((num1 + num2).toString());
+            else if (operator === "-") this.tokensArr.push((num1 - num2).toString());
+            else if (operator === "*") this.tokensArr.push((num1 * num2).toString());
+            else if (operator === "/") {
+                if (Math.trunc((num1 / num2) === 0)) this.tokensArr.push("0");
+                else this.tokensArr.push((Math.trunc(num1 / num2).toString()));
+            }
+        }
+    
+        getResult() {
+            return this.tokensArr.pop();
+        }
+    }
+    
+    const tokensInstance = new reversePolishNotation();
+    
+    for (let i = 0; i < tokens.length; i++) {
+        if (tokens[i] === "+" || tokens[i] === "-" || tokens[i] === "*" || tokens[i] === "/") {
+            tokensInstance.push(tokens[i]);
+            tokensInstance.getArithmeticResult();
+        } else {
+            tokensInstance.push(tokens[i]);
+        }
+    }
+    
+    return tokensInstance.getResult();
+};
