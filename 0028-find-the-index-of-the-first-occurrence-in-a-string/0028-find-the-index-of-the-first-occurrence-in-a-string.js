5ab5aa6bc3cdc2325347d10bb94efaa7202ce3fc
Bola Ghaly <59656591+BolaGhaly@users.noreply.github.com>
2024-08-08 01:12:25 -0400

    Time: 66 ms (7.36%), Space: 49.1 MB (24.16%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,18 +5,7 @@
  */
 var strStr = function(haystack, needle) {
     let newHaystackArr = [];
-    
-    // haystack.length = 9
-    // needle.length = 3
-    // sadbutsad
-    // i
-    // sad
-    // adb
-    // dbu
-    // but
-    // uts
-    // tsa
-    // sad
+
     for (let i = 0; i <= haystack.length - needle.length; i++) {
         newHaystackArr[i] = haystack.substring(i, i + needle.length);
     }
